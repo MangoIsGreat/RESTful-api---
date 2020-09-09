ea6e72927b5dacb1d2d5f4ac954cbd4816386f85
zp <859247268@qq.com>
2020-09-10 01:57:41 +0800

    实现登录功能，并且添加参数验证

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -12,7 +12,8 @@ class UsersCtl {
   }
   async update(ctx) {
     ctx.verifyParams({
-      name: { type: "string", required: true }
+      name: { type: "string", required: false },
+      password: { type: "string", required: false }
     })
     const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body)
     if (!user) { ctx.throw(404, "该用户不存在") }
@@ -20,8 +21,13 @@ class UsersCtl {
   }
   async create(ctx) {
     ctx.verifyParams({
-      name: { type: "string", required: true }
+      name: { type: "string", required: true },
+      password: { type: "string", required: true }
     })
+    // 获取请求体中的name属性：
+    const {name} = ctx.request.body
+    const repeatedUser = await User.findOne({name})
+    if (repeatedUser) {ctx.throw(409, '用户已占用')}
     const user = await new User(ctx.request.body).save()
     ctx.body = user
   }
